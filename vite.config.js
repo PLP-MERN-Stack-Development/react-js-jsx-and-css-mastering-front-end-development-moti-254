8cc5ccd8fc2a8d4aaafcc567f04924a4d7a8f109
moti-254 <timketer2020@gmail.com>
2025-11-15 11:44:04 +0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-js-jsx-and-css-mastering-front-end-development-moti-254/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
